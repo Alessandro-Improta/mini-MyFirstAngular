1e9b48cd06348774bda05e5d19cbf5d06dd77e48
Alessandro Improta <aimprota@Alessandros-MacBook-Air.local>
2017-06-03 11:49:46 -0600

    done

---
+++ js/mainCtrl.js
@@ -0,0 +1,3 @@
+angular.module('friendsList').controller('mainCtrl', function($scope) {
+	$scope.friends = ['Allyson Lois Lybbert', 'Jared Christian King', 'Julia Catherine King', 'Wyatt James Moore', 'Michael James Mullen', 'Zachary James Collier'];
+});
